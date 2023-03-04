68c24fac86da37cb627ca418e8342c5124c9256b
Newton Adhikari <neadh072@kathmandu>
2023-03-04 12:30:23 +0545

    9.3 completed

--- bmi-calculator/bmiCalculator.ts
+++ bmi-calculator/bmiCalculator.ts
@@ -1,5 +1,43 @@
-const calculateBmi = (height: number, weight: number) : string => {
-    return "Normal (helathy weight)";
+interface BodyFeatures {
+    height: number,
+    weight: number
 }
 
-console.log(calculateBmi(5.9, 60));
+const parseArguments = (params: string[]) : BodyFeatures => {
+    if(!params[2] || !params[3])
+        throw new Error("height or weight is missing: usage npm run CalculateBmi <height> <weight>");
+
+    if (isNaN(Number(params[2])) || isNaN(Number(params[3])))
+        throw new Error("height and weight must be number");
+
+    return {
+        height: Number(params[2]),
+        weight: Number(params[3])
+    }
+}
+
+const calculateBmi = (height: number, weight: number) : string => {
+    height = height / 100;
+    const bmi = weight / (height * height);
+    console.log(bmi);
+    const desc = bmi < 18.5 
+        ? "Underweight"
+        : bmi > 18.5 && bmi < 25 
+            ? "Normal (healthy weight)"
+            : "Overweight"
+    ;
+    return desc;
+}
+
+try {
+    const { height, weight } = parseArguments(process.argv);
+
+    console.log(calculateBmi(height, weight));
+}
+catch(error: unknown) {
+    let message = "error occured ";
+    if (error instanceof Error)
+        message += error.message;
+
+    console.log(message);
+}
--- bmi-calculator/exerciseCalculator.ts
+++ bmi-calculator/exerciseCalculator.ts
@@ -8,9 +8,36 @@ interface ExerciseResult {
     ratingDescription: string,
 }
 
-const calculateExercises = (dailExercises: number[], target: number) : ExerciseResult => {
-    const periodLength = 7;
-    const trainingDays = 5;
+interface TargetAndDaily {
+    target: number,
+    daily: number[]
+}
+
+const parseArgs = (params: string[]) : TargetAndDaily => {
+    if(!params[2] || !params[3])
+        throw new Error("target and exercises array missing: usage npm run CalculateExercises <target> <daily[]>");
+
+    let daily = params.slice(2);
+
+    let exercises: number[] = [];
+    
+    daily.forEach((n, i) => {
+        if (isNaN(Number(n))) throw new Error(`${n} is not a number`);
+        exercises[i] = Number(n);
+    })
+
+    return {
+        target: exercises[0],
+        daily: exercises.slice(1)
+    }
+}
+
+const calculateExercises = (target: number, dailExercises: number[]) : ExerciseResult => {
+    const periodLength = dailExercises.length;
+    const trainingDays = dailExercises.filter(e => {
+        if (e !== 0) return true
+        return false;
+    }).length;
     const average = dailExercises.reduce((a, b) => a + b, 0)/dailExercises.length;
     const success = average >= target;
     const rating = success ? 3 : 2;
@@ -30,4 +57,15 @@ const calculateExercises = (dailExercises: number[], target: number) : ExerciseR
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+    const { target, daily } = parseArgs(process.argv);
+
+    console.log(calculateExercises(target, daily));
+}
+catch(error: unknown) {
+    let message = "error occured ";
+    if (error instanceof Error)
+        message += error.message;
+
+    console.log(message);
+}
